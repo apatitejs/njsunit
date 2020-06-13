1e526aa0f166b65be3d0e56831f8aa5044b5c31e
pmadhur <pmadhur@hotmail.com>
2020-06-13 17:30:16 +0200

    Fixes #1

--- test/assertion-test-case.js
+++ test/assertion-test-case.js
@@ -16,7 +16,7 @@ class AssertionTestCase extends TestCase {
 
         return promise.then(() => {
             self.shouldThrow(() => {
-                self.assert(false)
+                self.assert(false, null, 'Failed assertion test in a promise.')
             }, TestFailure)
         })
     }
@@ -62,102 +62,102 @@ class AssertionTestCase extends TestCase {
 
     testAllFailAssertions() {
         this.shouldThrow(() => {
-            this.assert(1 === 2)
+            this.assert(1 === 2, null, 'Failed assertion test: When equality assertion fails, the default assertion text should be displayed.')
         }, TestFailure, 'Assertion failed.')
 
         this.shouldThrow(() => {
-            this.assert(1 === 2, '1 was expected to be equal to 2?')
+            this.assert(1 === 2, '1 was expected to be equal to 2?', 'Failed assertion test: When equality assertion fails, the custom assertion text should be displayed.')
         }, TestFailure, '1 was expected to be equal to 2?')
 
         this.shouldThrow(() => {
-            this.assertNull(24)
+            this.assertNull(24, null, 'Failed assertion test: When null assertion fails, the default assertion text should be displayed.')
         }, TestFailure, 'Assertion failed. Expected value to be null.')
 
         this.shouldThrow(() => {
-            this.assertNull(24, 'Value should have been null? No way.')
+            this.assertNull(24, 'Value should have been null? No way.', 'Failed assertion test: When null assertion fails, the custom assertion text should be displayed.')
         }, TestFailure, 'Value should have been null? No way.')
 
         this.shouldThrow(() => {
-            this.assertUndefined(24)
+            this.assertUndefined(24, null, 'Failed assertion test: When undefined assertion fails, the default assertion text should be displayed.')
         }, TestFailure, 'Assertion failed. Expected value to be undefined.')
 
         this.shouldThrow(() => {
-            this.assertUndefined(24, 'Value should have been undefined? No way.')
+            this.assertUndefined(24, 'Value should have been undefined? No way.', 'Failed assertion test: When undefined assertion fails, the custom assertion text should be displayed.')
         }, TestFailure, 'Value should have been undefined? No way.')
 
         this.shouldThrow(() => {
-            this.assertNullOrUndefined(24)
+            this.assertNullOrUndefined(24, null, 'Failed assertion test: When null or undefined assertion fails, the default assertion text should be displayed.')
         }, TestFailure, 'Assertion failed. Expected value to be either null or undefined.')
 
         this.shouldThrow(() => {
-            this.assertNullOrUndefined(24, 'Value should have been null or undefined? No way.')
+            this.assertNullOrUndefined(24, 'Value should have been null or undefined? No way.', 'Failed assertion test: When null or undefined assertion fails, the custom assertion text should be displayed.')
         }, TestFailure, 'Value should have been null or undefined? No way.')
 
         this.shouldThrow(() => {
-            this.assertNotNull(null)
+            this.assertNotNull(null, null, 'Failed assertion test: When not null assertion fails, the default assertion text should be displayed.')
         }, TestFailure, 'Assertion failed. Did not expect value to be null.')
 
         this.shouldThrow(() => {
-            this.assertNotNull(null, 'Value should have been NOT null? No way.')
+            this.assertNotNull(null, 'Value should have been NOT null? No way.', 'Failed assertion test: When not null assertion fails, the custom assertion text should be displayed.')
         }, TestFailure, 'Value should have been NOT null? No way.')
 
         this.shouldThrow(() => {
-            this.assertNotUndefined(undefined)
+            this.assertNotUndefined(undefined, null, 'Failed assertion test: When not undefined assertion fails, the default assertion text should be displayed.')
         }, TestFailure, 'Assertion failed. Did not expect value to be undefined.')
 
         this.shouldThrow(() => {
-            this.assertNotUndefined(undefined, 'Value should have been NOT undefined? No way.')
+            this.assertNotUndefined(undefined, 'Value should have been NOT undefined? No way.', 'Failed assertion test: When not undefined assertion fails, the custom assertion text should be displayed.')
         }, TestFailure, 'Value should have been NOT undefined? No way.')
 
         this.shouldThrow(() => {
-            this.assertNotNullOrUndefined(null)
+            this.assertNotNullOrUndefined(null, null, 'Failed assertion test: When not null or undefined assertion fails, the default assertion text should be displayed.')
         }, TestFailure, 'Assertion failed. Did not expect value to be either null or undefined.')
 
         this.shouldThrow(() => {
-            this.assertNotNullOrUndefined(null, 'Value should have been NOT null or undefined? No way.')
+            this.assertNotNullOrUndefined(null, 'Value should have been NOT null or undefined? No way.', 'Failed assertion test: When not null or undefined assertion fails, the custom assertion text should be displayed.')
         }, TestFailure, 'Value should have been NOT null or undefined? No way.')
 
         this.shouldThrow(() => {
-            this.assertNotNullOrUndefined(undefined)
+            this.assertNotNullOrUndefined(undefined, null, 'Failed assertion test: When not null or undefined (undefined case) assertion fails, the default assertion text should be displayed.')
         }, TestFailure, 'Assertion failed. Did not expect value to be either null or undefined.')
 
         this.shouldThrow(() => {
-            this.assertNotNullOrUndefined(undefined, 'Value should have been NOT null or undefined? No way.')
+            this.assertNotNullOrUndefined(undefined, 'Value should have been NOT null or undefined? No way.', 'Failed assertion test: When not null or undefined (undefined case) assertion fails, the custom assertion text should be displayed.')
         }, TestFailure, 'Value should have been NOT null or undefined? No way.')
 
         this.shouldThrow(() => {
-            this.assertEqual(1, 2)
+            this.assertEqual(1, 2, null, 'Failed assertion test: When equality assertion fails, the default assertion text should be displayed.')
         }, TestFailure, 'Assertion failed. Values should have been equal. Expected: "1", Actual: "2"')
 
         this.shouldThrow(() => {
-            this.assertEqual(1, 2, '1 == 2?')
+            this.assertEqual(1, 2, '1 == 2?', 'Failed assertion test: When equality assertion fails, the custom assertion text should be displayed.')
         }, TestFailure, '1 == 2?')
 
         this.shouldThrow(() => {
-            this.assertNotEqual(1, 1)
+            this.assertNotEqual(1, 1, null, 'Failed assertion test: When not equal assertion fails, the default assertion text should be displayed.')
         }, TestFailure, 'Assertion failed. Values should not have been equal. Expected: "1", Actual: "1"')
 
         this.shouldThrow(() => {
-            this.assertNotEqual(1, 1, '1 !== 1?')
+            this.assertNotEqual(1, 1, '1 !== 1?', 'Failed assertion test: When not equal assertion fails, the custom assertion text should be displayed.')
         }, TestFailure, '1 !== 1?')
 
         var self = this
         this.shouldThrow(() => {
             self.shouldThrow(() => {
                 foo.bar()
-            }, SyntaxError)
+            }, SyntaxError, null, 'Fail when wrong error class instead of expected error class.')
         }, TestFailure, 'Should have thrown an error of class SyntaxError. But ReferenceError was thrown.')
 
         this.shouldThrow(() => {
             self.shouldThrow(() => {
                 self.assert(25 === 25)
-            }, ReferenceError)
+            }, ReferenceError, null, 'Fail when wrong error class instead of expected error class.')
         }, TestFailure, 'Should have thrown an error of class ReferenceError. But no error was thrown.')
 
         this.shouldThrow(() => {
             self.shouldThrow(() => {
                 foo.bar()
-            }, ReferenceError, 'bar is not defined')
+            }, ReferenceError, 'bar is not defined', 'Fail when error is thrown with a different expected error test.')
         }, TestFailure, 'Error was thrown as expected but the error message was "foo is not defined" instead of "bar is not defined".')
 
         this.shouldThrow(() => {
